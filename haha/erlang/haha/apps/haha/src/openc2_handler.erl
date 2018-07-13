a732a2e925a42a6cbc995adf1f14245d49a385df
Duncan Sparrell <duncan@sfractal.com>
2018-07-13 10:23:31 -0400

    first level of query specifications

--- haha/erlang/haha/apps/haha/src/openc2_handler.erl
+++ haha/erlang/haha/apps/haha/src/openc2_handler.erl
@@ -232,10 +232,52 @@ structure_check( true  % IdKeyExists
     ActionBin = maps:get( <<"action">>, JsonMap ),
     lager:info("ActionBin ~p", [ActionBin]),
     TargetBin = maps:get( <<"target">>, JsonMap ),
-    lager:info("ActionBin ~p", [ActionBin]),
-    query_check(ActionBin, TargetBin, Req, State).
+    lager:info("TargetBin ~p", [TargetBin]),
+    %% parse the type of target so can select on it
+    %%  TargetInfo = { TargetIsBinary, TargetIsMap, TopTarget, SpecifierList}
+    TargetInfo = target_typing(TargetBin),
+    lager:info("sc: TargetInfo = ~p", [TargetInfo]),
+    query_check(ActionBin, TargetInfo, Req, State).
 
-query_check(<<"query">>, <<"whatareyou">>, Req, State) ->
+target_typing(TargetBin) ->
+    TargetIsBinary = is_binary(TargetBin),
+    TargetIsMap = is_map(TargetBin),
+    {TopTarget, SpecifierList} = string_map( TargetIsBinary
+                                           , TargetIsMap
+                                           , TargetBin
+                                           ),
+    {TargetIsBinary, TargetIsMap, TopTarget, SpecifierList}.
+
+string_map(true, _TargetIsMap, TargetBin) ->
+    TopTarget = TargetBin,
+    SpecifierList = [],
+    {TopTarget, SpecifierList};
+string_map(false, true, TargetBin) ->
+    lager:info("stringmap TargetBin as map"),
+    TargetList = maps:keys(TargetBin),
+    %% error if not one and only one target in map
+    case length(TargetList) of
+        0 ->
+          {error, ["only one target allowed"]};
+        1 ->
+          %% the one item is the target
+          TopTarget = lists:nth(1,TargetList),
+          SpecifierMap = maps:get(TopTarget,TargetBin),
+          SpecifierList = maps:keys(SpecifierMap),
+          {TopTarget, SpecifierList};
+        _ ->
+          {error, ["only one target allowed"]}
+    end;
+string_map(false, false, _) ->
+    %% bad target value since neither string nor map
+    {error, ["bad target json"]}.
+
+%% query_check
+%%  query_check(ActionBin, TargetInfo, Req, State)
+%%    TargetInfo = {TargetIsBinary, TargetIsMap, TopTarget, SpecifierList}
+%%
+%% case where ActionBin=query, TargetIsBinary=true, target= whatareyou
+query_check(<<"query">>, {true, false, <<"whatareyou">>, []}, Req, State) ->
     %% query action, whatareyou target
     %%figure out good reply;
     HelloWorld = <<"Hello World">>,
@@ -245,23 +287,41 @@ query_check(<<"query">>, <<"whatareyou">>, Req, State) ->
                            , OuputJson
                            , Req
                            ),
+    lager:info("query_check whatareyou"),
+    {true, Req2, State};
 
-    {ok, Req2, State};
-
-query_check(<<"query">>, <<"profile">>, Req, State) ->
+%% case where ActionBin=query, TargetIsBinary=true, target= openc2,
+%%     with no target specifiers (do return all)
+query_check(<<"query">>, {true, false, <<"openc2">>, []}, Req, State) ->
     %% figure out
-    HelloWorld = <<"Need to figure this part out still">>,
+    HelloWorld = <<"Need to figure out openc2=all still">>,
     OuputJson = jsx:encode(HelloWorld),
     Req2 = cowboy_req:reply( 200
                            , #{<<"content-type">> => <<"application/json">>}
                            , OuputJson
                            , Req
                            ),
-    {ok, Req2, State};
+    lager:info("query_check openc2 binary"),
+    {true, Req2, State};
+
+%% case where ActionBin=query, TargetIsMap=true, target= openc2,
+%%     with target specifiers
+query_check(<<"query">>, {false, true, <<"openc2">>, SpecList}, Req, State) ->
+  %% figure out
+  lager:info("query_check SpecList ~p", [SpecList]),
+  HelloWorld = <<"Need to figure out openc2=list still">>,
+  OuputJson = jsx:encode(HelloWorld),
+  Req2 = cowboy_req:reply( 200
+                         , #{<<"content-type">> => <<"application/json">>}
+                         , OuputJson
+                         , Req
+                         ),
+  lager:info("query_check openc2 map"),
+  {true, Req2, State};
 
 query_check(<<"query">>, _Target, Req, State) ->
     %% Bad target
-    ErrorMsg = <<"Bad Target, only profile, whatareyou supported">>,
+    ErrorMsg = <<"Bad Target">>,
     Req2 = cowboy_req:reply( 400
                            , #{<<"content-type">> => <<"text/html">>}
                            , ErrorMsg
@@ -275,7 +335,7 @@ query_check(_Action, _Target, Req, State) ->
     %% bad action
     Req2 = cowboy_req:reply( 400
                            , #{<<"content-type">> => <<"text/html">>}
-                           , <<"Bad Action, only query supported">>
+                           , <<"Bad Action">>
                            , Req
                            ),
     %% return (don't move on since request was bad)
